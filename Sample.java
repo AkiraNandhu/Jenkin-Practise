19d6270bffc9adc1c37feaae765449aed1c65207
AkiraNandhu <40230072+AkiraNandhu@users.noreply.github.com>
2020-06-01 15:54:16 +0530

    Intial commit

---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class Sample
+{
+  public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Jenkins");
+		System.out.println("Enjoy ur journey in jenkins in collaboration with github !.....");
+		
+	}
+}
